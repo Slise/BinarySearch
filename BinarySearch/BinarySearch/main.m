b01f7f20e5469402c718505434c6379d79951137
Benson Huynh <quoc@shaw.ca>
2015-10-20 17:22:39 -0700

    1st save

---
+++ BinarySearch/BinarySearch/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  BinarySearch
+//
+//  Created by Benson Huynh on 2015-10-20.
+//  Copyright © 2015 Benson Huynh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int binarySearch(int target, ) {
+    
+}
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSArray *sortedNumbers = @[@"3", @"4", @"7", @"9", @"11", @"19", @"22", @"27"];
+        
+        binarySearch(int 4) = sortedNumbers;
+        
+        if (found) {
+            NSLog(@"%d", found);
+            
+        }
+
+        
+        
+    }
+    return 0;
+}
